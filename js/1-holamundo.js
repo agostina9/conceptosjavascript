c7b81a24ec21193c7dc2b557091c1103f3f1de93
Agostina <lozagostina@gmail.com>
2022-06-28 18:03:32 -0300

    agregamos mensaje por diferentes pantallas

---
+++ js/1-holamundo.js
@@ -0,0 +1,13 @@
+//esto es un comentario de una unica linea
+
+/*esto es un comentario
+de multiples lineas*/
+
+//mostrar un mensaje en la pantala o documento
+document.write('hola comision');
+
+//mostrar un mensaje por consola
+console.log('prueba de consola');
+
+//mostrar un mensaje en una ventna emergente
+alert('desde una ventana emergente');
