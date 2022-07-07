5cdf69e6e66d5816be14ffe2c45d1c990bd96893
Agostina <lozagostina@gmail.com>
2022-07-07 01:33:51 -0300

    empezamos con bucles, agrego primer ejemplo

---
+++ js/3-bucles.js
@@ -0,0 +1,15 @@
+/* Bucles
+
+while(condicion logica){
+    todas las lineas de codigo que quiero que repita el programa
+    cambiar la condicion logica
+}
+
+*/
+
+let cantidad = 0;
+
+while(cantidad <= 20){
+    document.write('<br>preparar mate y entregarle al sig' + cantidad)
+    cantidad++
+}
